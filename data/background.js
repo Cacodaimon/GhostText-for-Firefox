b77663d1c11fbb7df1d6463fed007b2a7be77a4e
Guido Kroemer <mail@cacodaemon.de>
2014-08-30 22:31:33 +0200

    First code cleanup, not working yet…

--- data/background.js
+++ data/background.js
@@ -1,78 +1,123 @@
+'use strict';
+
 /**
- * Tab id to WebSocket mapping.
+ * GhostText for FireFox background script.
  *
- * @type {Array<WebSocket>}
+ * @licence The MIT License (MIT)
+ * @author Guido Krömer <mail 64 cacodaemon 46 de>
+ * @author Federico Brigante
  */
-var webSockets = [];
+var GhostTextBackground = {
+    /**
+     * Tab id to WebSocket mapping.
+     *
+     * @type {Array<WebSocket>}
+     */
+    webSockets: [],
 
-function connect(message) {
-    console.log('InBackground.connect');
+    /**
+     * Creates a new connection the GhostText server WebSocket at the given port.
+     *
+     * @param {*} message
+     */
+    connect: function(message) {
+        console.log('GhostTextBackground.connect');
 
-    if (message.response.status !== 200) {
-        console.log("Connect error?");
+        if (message.response.status !== 200) {
+            console.log('Connect error?');
 
-        return;
-    }
-    var response = JSON.parse(message.response.responseText);
+            return;
+        }
+        var response = JSON.parse(message.response.responseText);
 
-    if (response.ProtocolVersion != 1) {
-        console.log("Wrong version?");
+        if (response.ProtocolVersion != 1) {
+            console.log('Wrong version?');
 
-        return;
-    }
+            return;
+        }
 
-    var webSocket = new WebSocket('ws://localhost:' + response.WebSocketPort);
+        var webSocket = new WebSocket('ws://localhost:' + response.WebSocketPort);
 
-    webSocket.onopen = function () {
-        console.log('webSocket.onopen');
-        webSocket.send(message.originMessage.change);
-        console.log('webSocket.onopen.send: ' + message.originMessage.change);
-    };
+        webSocket.onopen = function() {
+            console.log('webSocket.onopen');
+            webSocket.send(message.originMessage.change);
+            console.log('webSocket.onopen.send: ' + message.originMessage.change);
+        };
 
-    webSocket.onmessage = function (event) {
-        self.postMessage({
-            tabId: message.tabId,
-            type: 'textChange',
-            change: event.data
+        webSocket.onmessage = function(event) {
+            self.postMessage({
+                tabId: message.tabId,
+                type: 'textChange',
+                change: event.data
+            });
+        };
+
+        webSocket.onclose = function () {
+            delete GhostTextBackground.webSockets[message.tabId.toString()];
+
+            //TODO send clode msg
+        };
+
+        GhostTextBackground.webSockets[message.tabId.toString()] = webSocket;
+    },
+
+    /**
+     * Closes the connection.
+     *
+     * @param message
+     */
+    disconnect: function(message) {
+        console.log('GhostTextBackground.disconnect');
+
+        try {
+            GhostTextBackground.webSockets[message.tabId.toString()].close();
+        } catch (e) {
+            console.log(e)
+        }
+
+        delete GhostTextBackground.webSockets[message.tabId.toString()];
+    },
+
+    /**
+     * Sends a text change to the GhostText server.
+     *
+     * @param message
+     */
+    textChange: function(message) {
+        console.log('GhostTextBackground.textChange');
+
+        console.log(message);
+        console.log(GhostTextBackground.webSockets[message.tabId.toString()]);
+        GhostTextBackground.webSockets[message.tabId.toString()].send(message.change);
+    },
+
+    /**
+     * Init the on message handler, waiting for incoming messages from the main.js script.
+     */
+    init: function () {
+        console.log('GhostTextBackground.init');
+
+        self.on('message', function(message) {
+            console.log(message.type);
+
+            switch (message.type) {
+                case 'connect':
+                    GhostTextBackground.connect(message);
+                    break;
+
+                case 'disconnect':
+                    GhostTextBackground.disconnect(message);
+                    break;
+
+                case 'textChange':
+                    GhostTextBackground.textChange(message);
+                    break;
+
+                default:
+                    console.log(['Unknown message of type', message.type, 'given'].join(' '));
+            }
         });
-    };
-
-    webSockets[message.tabId.toString()] = webSocket;
-}
-
-function disconnect(message) {
-    try {
-        webSockets[message.tabId.toString()].close();
-    } catch (e) {
-        console.log(e)
     }
+};
 
-    delete webSockets[message.tabId.toString()];
-}
-
-function textChange(message) {
-    console.log(message);
-    console.log(webSockets[message.tabId]);
-    webSockets[message.tabId].send(message.change);
-}
-
-self.on('message', function(message) {
-    console.log('InBackgroundOnMessage');
-    console.log(message.type);
-
-    switch (message.type) {
-        case 'connect':
-            connect(message);
-            break;
-
-        case 'disconnect':
-            disconnect(message);
-            break;
-
-        case 'textChange':
-            textChange(message);
-            break;
-    }
-});
-
-console.log("Background script injected");
+GhostTextBackground.init();
--- data/content.js
+++ data/content.js
@@ -1,4 +1,4 @@
-var textChange = function (title, textArea, tabUrl) {
+/*var textChange = function (title, textArea, tabUrl) {
     var textAreaDom = $(this).get(0);
 
     return JSON.stringify({
@@ -53,13 +53,72 @@ if ($focusedTextarea.length == 1) {
         $textAreas.val(response.text);
 
         var textArea = $($textAreas).get(0);
-        /** @type {{start: {number}, end: {number}}} */
-        var minMaxSelection = getMinMaxSelection.getMinMaxSelection(response.selections);
-        textArea.selectionStart = minMaxSelection.start;
-        textArea.selectionEnd   = minMaxSelection.end;
+        // @type {{start: {number}, end: {number}}}
+        //var minMaxSelection = getMinMaxSelection(response.selections);
+        //textArea.selectionStart = minMaxSelection.start;
+        //textArea.selectionEnd   = minMaxSelection.end;
         textArea.focus();
     });
+}*/
+
+/** @type InputArea */
+var currentInputArea = null;
+
+var reportFieldData = function () {
+    console.log('GhostText: reportFieldData()');
+
+    if (currentInputArea === null) {
+        throw 'reportFieldData as been called without initializing currentInputArea!';
+    }
+
+    /** @type TextChange */
+    var textChange = currentInputArea.buildChange();
+
+    self.postMessage({
+        tabId: self.options.tab.id,
+        change: JSON.stringify(textChange),
+        type: 'textChange',
+        recipient: 'background'
+    });
+};
+
+self.on('message', function(message) {
+    var response = JSON.parse(message.change);
+    currentInputArea.setText(response.text);
+});
+
+var detector = new GhostText.InputArea.Detector();
+detector.focusEvent(function (inputArea) {
+    console.log('GhostText: detector.focusEvent()');
+    currentInputArea = inputArea;
+
+    inputArea.textChangedEvent(function () { reportFieldData();});
+    inputArea.removeEvent(function () { alert('TODO remove evt'); });
+    inputArea.unloadEvent(function () { alert('TODO remove evt'); });
+    inputArea.focusEvent(null); //disable
+    inputArea.selectionChangedEvent(null);
+
+    /** @type TextChange */
+    var textChange = currentInputArea.buildChange();
+
+    self.postMessage({
+        tabId: self.options.tab.id,
+        change: JSON.stringify(textChange),
+        type: 'connect',
+        recipient: 'main'
+    });
+
+    reportFieldData(); //Report initial content of field
+});
+
+var countElementsFound = detector.detect(document);
+if (countElementsFound === 0) {
+    alert('No text area elements on this page');
+} else if (countElementsFound > 1) {
+    alert('There are multiple text areas on this page. \n Click on the one you want to use.');
 }
+
+
 //TODO error, onclose, messages…
 
-alert("Content script injected");
+alert("Content script injected… 2000");
