a625b332cf6aa6a82580fff1bb3ee0dc447b9172
Guido Kroemer <mail@cacodaemon.de>
2014-09-03 22:12:20 +0200

    Notifications use humane.js or native notifications. #4

--- data/content.js
+++ data/content.js
@@ -1,3 +1,53 @@
+'use strict';
+
+/**
+ * GhostText for FireFox content script.
+ *
+ * @licence The MIT License (MIT)
+ * @author Guido Krömer <mail 64 cacodaemon 46 de>
+ * @author Federico Brigante
+ */
+var GhostTextContent = {
+    /**
+     * The field we or the user selected.
+     *
+     * @type IInputArea
+     */
+    currentInputArea: null,
+
+    /**
+     * Displays the passed message to the user.
+     *
+     * @param  {string}  message Message to display
+     * @param  {boolean} stay    Whether the message will stay on indefinitely
+     * @private
+     * @static
+     */
+    informUser: function (message, stay) {
+        console.info('GhostText:', message);
+        GThumane.remove();
+
+        message = message.replace(/\n/g,'<br>');
+        var timeout = stay ? 0 : GhostTextContent.getMessageDisplayTime(message);
+        GThumane.log(message, {
+            timeout: timeout,
+            clickToClose: true
+        });
+    },
+
+    /**
+     * Gets how long a message needs to stay on screen
+     *
+     * @param  {string} message Message to display
+     * @return {number} The duration in milliseconds
+     */
+    getMessageDisplayTime: function (message) {
+        var wpm = 100;//180 is the average words read per minute, make it slower
+
+        return message.split(' ').length / wpm * 60000;
+    }
+};
+
 /** @type InputArea */
 var currentInputArea = null;
 
@@ -20,8 +70,20 @@ var reportFieldData = function () {
 };
 
 self.on('message', function(message) {
-    var response = JSON.parse(message.change);
-    currentInputArea.setText(response.text);
+    switch (message.type) {
+        case 'textChange':
+            var response = JSON.parse(message.change);
+            currentInputArea.setText(response.text);
+            break;
+        case 'close':
+            currentInputArea.unbind();
+            currentInputArea = null;
+            informUser('Disconnected! \n <a href="https://github.com/Cacodaimon/GhostTextForChrome/issues?state=open" target="_blank">Report issues</a> | <a href="https://chrome.google.com/webstore/detail/sublimetextarea/godiecgffnchndlihlpaajjcplehddca/reviews" target="_blank">Leave review</a>');
+            break;
+        default:
+            console.log(['Unknown message of type', message.type, 'given'].join(' '));
+    }
+
 });
 
 var detector = new GhostText.InputArea.Detector(GhostText.InputArea.Browser.Firefox);
@@ -50,11 +112,9 @@ detector.focusEvent(function (inputArea) {
 
 var countElementsFound = detector.detect(document);
 if (countElementsFound === 0) {
-    alert('No text area elements on this page');
+    GhostTextContent.informUser('No text area elements on this page');
 } else if (countElementsFound > 1) {
-    alert('There are multiple text areas on this page. \n Click on the one you want to use.');
+    GhostTextContent.informUser('There are multiple text areas on this page. \n Click on the one you want to use.');
 }
 
 //TODO error, onclose, messages…
-
-alert("Content script injected… 2000");
